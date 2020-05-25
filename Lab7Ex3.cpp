f147fd60b8ac3402d5d737f3b1826e328592cdcc
Hongzhuo CHEN <richardhzchen@outlook.com>
2020-05-25 14:23:13 +0800

    Add files via upload

---
+++ Lab7Ex3.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stdexcept>
+#include<string>
+using namespace std;
+class TestException:runtime_error{
+public:
+	TestException(string s = "")
+		:runtime_error(s){}
+};
+void g()
+{
+	try {
+		throw TestException();
+	}
+	catch (TestException&)
+	{
+		cout << "Exception caught in function g(). Rethrowing...\n";
+		throw;
+	}
+}
+int main()
+{
+	try {
+		g();
+	}
+	catch (TestException&)
+	{
+		cout << "Exception caught in function main().";
+	}
+	return 0;
+}
